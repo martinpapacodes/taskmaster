20003ba84e6f577d930293098485762acb450855
Papa <martipap@8c8590210f1e.ant.amazon.com>
2020-02-12 10:25:02 -0800

    finished displaying username

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static String TAG = "mnf.main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,34 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(goToAddATaskPage);
             }
         });
+
+
+        Button settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToSettingsPage = new Intent(MainActivity.this, settings.class);
+                MainActivity.this.startActivity(goToSettingsPage);
+
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Activity Resumed");
+
+        TextView usernameTitle = findViewById(R.id.usernameTitle);
+        SharedPreferences  sharedPref =  PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String username = sharedPref.getString("username", usernameTitle.getText().toString());
+        Log.d(TAG, username);
+        usernameTitle.setText(username + "'s tasks");
+    }
+
+
+
+
 }
